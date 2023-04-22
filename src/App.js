f7aee101719ad5f865fc1c4018efb5ee48f2b5b6
rScherpel <scherpelrafa@gmail.com>
2023-04-22 18:37:11 -0300

    feat: js logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import "./styles.css";
-export default function App() {
 
+export default function App() {
+  
   // White part movement effect
   useEffect(() => {
     const yourList = document.querySelector(".addBtn");
@@ -19,6 +20,21 @@ export default function App() {
     };
   }, []);
 
+  // To do list part
+  const [data, setData] = useState(() => {
+    const storedData = localStorage.getItem("data");
+    return storedData ? JSON.parse(storedData) : [];
+  });
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setData([...data, { title: newTitle, description: newDescription }]);
+    setNewTitle();
+    setNewDescription();
+    localStorage.setItem("data", JSON.stringify([...data, { title: newTitle, description: newDescription }]));
+  };
   return (
     <div className="container">
       <div className="bg-bx">
@@ -30,6 +46,17 @@ export default function App() {
         </div>
         <div className="box preview-bx">
           <h2>Preview</h2>
+          <div>
+            {data.map((item, index) => {
+              const key = item.title + "-" + item.description;
+              return (
+                <div className="taskContainer" key={key}>
+                  <p className="Title">{item.title}</p>
+                  <p className="description">{item.description}</p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
       <div className="listBx">
@@ -42,16 +69,28 @@ export default function App() {
           <div id="header-title">
             <h2>Criar Tarefa</h2>
           </div>
-          <form>
-            <input type="text" name="title" placeholder="Título" />
-            <input type="text" name="description" placeholder="Descrição" />
-            <button>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={newTitle}
+              onChange={(event) => setNewTitle(event.target.value)}
+              placeholder="Titulo"
+            />
+            <input
+              type="text"
+              value={newDescription}
+              onChange={(event) => setNewDescription(event.target.value)}
+              placeholder="descrição"
+            />
+            {/* <input type="time" name="date" /> */}
+            <button type="submit" id="sendTaskBtn">
               <img src="./images/send.svg" />
             </button>
           </form>
         </div>
         <div className="done-todo">
           <h2>Tarefas feitas</h2>
+          <button></button>
         </div>
       </div>
     </div>
